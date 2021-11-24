40b96d643167135f85695c78b32cb5cca5a89e83
STATION 0 <STATION 0@STATION0-PC>
2021-11-24 13:40:07 +0300

    Добавьте файлы проекта.

---
+++ base1_Lesson6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base1_Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ввести с клавиатуры предложение. Предложение - слова, разделенные пробелом. Знаки препинания не использвать.");
+            string aS = Console.ReadLine();
+            string[] arrayS = aS.Split();
+            int countWord = 0, maxWord = 0;// Начальные значения счетчиков массива и наиб. слова
+            Console.WriteLine();
+                for (int i = 0; i < arrayS.Length; i++)
+                {
+                    if (arrayS[i].Length > maxWord)
+                    {
+                    maxWord = arrayS[i].Length; // определение макс. кол-ва символов в массиве слов
+                    countWord = i; //! счетчик индекса массива с наиб. длинной слова
+                    }
+                }
+            Console.WriteLine(" Самое длинное слово в строке - {0} ", arrayS[countWord]);
+            Console.ReadKey();
+        }
+    }
+}
